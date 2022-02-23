649a61cf4700db300e0cb8ffe1cc76d5b67eb3ca
Trung Huỳnh <19520317@ms.uit.edu.vn>
2022-02-24 01:44:23 +0700

    Feat: get user by id

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -3,41 +3,50 @@ const AppError = require('../../utils/AppError');
 const catchAsync = require('../../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const {
-  getVerificationRequests,
-  updateVerificationRequest,
+	getVerificationRequests,
+	updateVerificationRequest,
 } = require('./userVerificationController');
 
 //@desc         get user information
 //@route        GET /api/users
 //@access       PRIVATE
 const getUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find({}).lean();
-  res.status(200).json({
-    users,
-  });
+	const users = await User.find({}).lean();
+	res.status(200).json({ users });
+});
+
+//@desc         get user information
+//@route        GET /api/users
+//@access       PRIVATE
+const getUser = catchAsync(async (req, res, next) => {
+	const { _id } = req.params;
+
+	const user = await User.findById({ _id });
+	res.status(200).json(user);
 });
 
 //@desc         get user information
 //@route        GET /api/users
 //@access       PUBLIC
 const login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const user = await User.findOne({ email, role: 'admin' });
-  if (!user || password !== process.env.ADMIN_PASSWORD) {
-    return next(AppError('Email hoặc mật khẩu không hợp lệ', 404));
-  }
+	const user = await User.findOne({ email, role: 'admin' });
+	if (!user || password !== process.env.ADMIN_PASSWORD) {
+		return next(AppError('Email hoặc mật khẩu không hợp lệ', 404));
+	}
 
-  res.status(200).json({
-    accessToken: jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    }),
-  });
+	res.status(200).json({
+		accessToken: jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+			expiresIn: process.env.JWT_EXPIRES_IN,
+		}),
+	});
 });
 
 module.exports = {
-  getUsers,
-  login,
-  getVerificationRequests,
-  updateVerificationRequest,
+	getUsers,
+	getUser,
+	login,
+	getVerificationRequests,
+	updateVerificationRequest,
 };
